7ba8a2f2aee86856f50060b5b07c24ed67c19566
hanwoo123 <hswnhj09@naver.com>
2024-04-10 18:50:55 +0900

    20240410ProjectFirstCommit

---
+++ RobotStoryGame/Assets/scripts/Joystick.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Joystick : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler
+{
+    private Image imageBackground;
+    private Image imageController;
+    private void Awake()
+    {
+        imageBackground = GetComponent<Image>();
+        imageController = transform.GetChild(0).GetComponent<Image>();
+    }    
+    public void OnPointerDown(PointerEventData eventData)
+    {
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector2 touchPosition = Vector2.zero;
+
+        if( RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            imageBackground.rectTransform, eventData.position, eventData.pressEventCamera, out touchPosition ) )
+        {
+            touchPosition.x = (touchPosition.x / imageBackground.rectTransform.sizeDelta.x);
+            touchPosition.y = (touchPosition.y / imageBackground.rectTransform.sizeDelta.y);
+
+            touchPosition = new Vector2(touchPosition.x * 2 - 1, touchPosition.y * 2 - 1);
+
+            touchPosition = (touchPosition.magnitude > 1) ? touchPosition.normalized : touchPosition;
+
+            imageController.rectTransform.anchoredPosition = new Vector2(
+                touchPosition.x * imageBackground.rectTransform.sizeDelta.x / 2,
+                touchPosition.y * imageBackground.rectTransform.sizeDelta.y / 2);
+        }
+
+    }
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        imageController.rectTransform.anchoredPosition = Vector2.zero;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

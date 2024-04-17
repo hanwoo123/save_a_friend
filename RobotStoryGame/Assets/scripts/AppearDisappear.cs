9b6df213069566636ff32989fd9c4969ed77971b
hanwoo123 <hswnhj09@naver.com>
2024-04-17 17:04:46 +0900

    20240417 comseba commit

--- RobotStoryGame/Assets/scripts/AppearDisappear.cs
+++ RobotStoryGame/Assets/scripts/AppearDisappear.cs
@@ -4,28 +4,28 @@ using UnityEngine;
 
 public class AppearDisappear : MonoBehaviour
 {
-    public float limit_time = 3.0f;
+    public float limit_time = 1.0f;
     public float current_time = 0.0f;
-    public bool isEnable;
-
+    public bool isEnable = false;
+    public float set_x = 1.0f;
+    public float set_y = 1.0f;
     // Start is called before the first frame update
     void Start()
     {
-        isEnable = false;
-        gameObject.SetActive(false);
-        Debug.Log("mark1");
+
     }
 
     // Update is called once per frame
     void Update()
     {
         current_time += Time.deltaTime;
-        Debug.Log("mark2"+current_time);
         if (limit_time <= current_time)
         {
-            Debug.Log("mark3");
-            isEnable = true;
-            gameObject.SetActive(isEnable);
+            if (!isEnable)
+            {
+                gameObject.transform.Translate(new Vector3(set_x, set_y, 0f));
+                isEnable = true;
+            }
             current_time = 0.0f;
         }
     }
